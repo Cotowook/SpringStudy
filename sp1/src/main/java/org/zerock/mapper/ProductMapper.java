152ec7dd58b948842186474cd9557e65f8781dba
cotowook <cotowook@naver.com>
2026-03-03 11:50:13 +0900

    특정 상품 조회 테스트 완료

--- sp1/src/main/java/org/zerock/mapper/ProductMapper.java
+++ sp1/src/main/java/org/zerock/mapper/ProductMapper.java
@@ -1,9 +1,14 @@
 package org.zerock.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.zerock.dto.ProductDTO;
 
 public interface ProductMapper {
 	int insert(ProductDTO productDTO);
 	
-	int insertImage(ProductDTO productDTO);
+	int insertImages(ProductDTO productDTO);
+	
+	ProductDTO selectOne(@Param("pno") Integer pno);
+	
+	int deleteOne(@Param("pno") Integer pno);
 }
---
+++ sp1/src/test/java/org/zerock/mapper/ProductMapperTests.java
@@ -0,0 +1,61 @@
+package org.zerock.mapper;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.dto.ProductDTO;
+
+import lombok.extern.log4j.Log4j2;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j2
+public class ProductMapperTests {
+	@Autowired
+	private ProductMapper productMapper;
+	
+	// 테스트의 transactional의 기본 동작 
+	// 1. 테스트 시작 -> 트랜잭션 시작 
+	// 2. 테스트 종료 -> 무조건 롤백
+	// 	=> 테스트는 DB를 더럽히지 않는 것이 기본 철학
+	@Transactional
+	@Commit	// 테스트 트랜잭션을 롤백하지 않고 commit 하기 위한 어노테이션
+	@Test
+	public void testInsert() {
+		ProductDTO productDTO = ProductDTO.builder()
+				.pname("product")
+				.pdesc("product desc")
+				.writer("user1")
+				.price(4000)
+				.build();
+		
+		// 상품 테이블에 등록
+		productMapper.insert(productDTO);
+		
+		// 상품 이미지 테이블에 등록
+		productDTO.addImage(UUID.randomUUID().toString(), "test1.jpg");
+		productDTO.addImage(UUID.randomUUID().toString(), "test2.jpg");
+
+		productMapper.insertImages(productDTO);
+	}
+	
+	@Test
+	public void testSelectOne() {
+		Integer pno = 1;
+		
+		ProductDTO dto = productMapper.selectOne(pno);
+		
+		log.info(dto);
+		dto.getImageList().forEach(log::info);
+	}
+	
+	
+	
+	
+}
